bd921e0846727c043fe2fb9e3f72afba598e4eb0
Tom <thomas.spencer@codurance.com>
2020-05-04 00:55:59 +0100

    Add extra functionality for stopping scroll when the modal is open

--- index.js
+++ index.js
@@ -19,10 +19,21 @@ var stopVideo = function ( element ) {
 
 modalBtn.addEventListener('click', function(){
   modalBg.classList.add('bg-active');
+  // When the modal is shown, we want a fixed body
+  document.body.style.position = 'fixed';
+  document.body.style.top = `-${window.scrollY}px`;
 });
 
 modalClose.addEventListener('click', function(){
   modalBg.classList.remove('bg-active');
   stopVideo(modal);
+  // When the modal is hidden, we want to remain at the top of the scroll position
+  document.body.style.position = '';
+  document.body.style.top = '';
+  // When the modal is hidden...
+  const scrollY = document.body.style.top;
+  document.body.style.position = '';
+  document.body.style.top = '';
+  window.scrollTo(0, parseInt(scrollY || '0') * -1);
 });
 
